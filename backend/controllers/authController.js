e5003af548e8a91267c14575ca29025d1a517a91
abhinav84094 <abhinav84094@gmail.com>
2026-07-03 09:02:51 +0530

    implement Google OAuth authentication with JWT

---
+++ backend/controllers/authController.js
@@ -0,0 +1,80 @@
+import { OAuth2Client } from "google-auth-library";
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const googleLogin = async (req, res) => {
+    try {
+
+        const { token } = req.body;
+
+        if (!token) {
+            return res.status(400).json({
+                success: false,
+                message: "Google token is required"
+            });
+        }
+
+        // Verify Google ID Token
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload();
+
+        const {
+            sub,
+            email,
+            name,
+            picture
+        } = payload;
+
+        // Find existing user
+        let user = await User.findOne({ email });
+
+        if (!user) {
+
+            user = await User.create({
+                googleId: sub,
+                name,
+                email,
+                picture,
+            });
+
+        } else {
+            user.name = name;
+            user.picture = picture;
+            user.googleId = sub;
+
+            await user.save();
+        }
+
+        // Generate JWT
+        const jwtToken = generateToken(user);
+
+        // Store in cookie
+        res.cookie("token", jwtToken, {
+            httpOnly: true,
+            secure: false,      // true after deployment
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.status(200).json({
+            success: true,
+            user,
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,32 @@ import express from "express"
 import dotenv from "dotenv"
 import DBconnection from "./db/mongodb.js";
 import userRoutes from "./routes/userRoutes.js"
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js"
+import cors from "cors"
+
+
+dotenv.config();
 
 const app = express();
 
-dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: "http://127.0.0.1:5173",
+        credentials: true,
+    })
+);
+
+
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res)=>{
-    res.send("this is home page").status(200)
+    res.send("this is home page")
 })
 
 
